de9ea1ace71b7562f1157f0983ac94bed4d44ca7
Germán Sartori <germansartori108@gmail.com>
2024-07-07 17:25:00 -0300

    cambio de barrita 4

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 
-# Ruta del directorio actual del script
+
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Rutas relativas
 movies_path = os.path.join(current_dir, "datos_procesados", "movies_df.csv")
+
 director_actor_path = os.path.join(current_dir, "datos_procesados", "director_actor_df.csv")
 
-# Carga de los datasets
-df_movies_limpio = pd.read_csv(movies_path)
-director_actor_df = pd.read_csv(director_actor_path, index_col=0)
+
 
 
 # Endpoint cantidad_filmaciones_mes
